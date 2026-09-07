1bd68106c4a999d9f02c4589b2e02ff5f85b63e8
Alejandro Figueroa <cybrvybe@gmail.com>
2026-09-06 20:11:16 -0400

    Fix browser preset loader

--- src/presets.js
+++ src/presets.js
@@ -1,2 +1,2 @@
 'use strict';
-document.write('<script src="src/presets-glsl.js"><\\/script><script src="src/presets-data.js"><\\/script><script src="src/presets-runtime.js"><\\/script>');
+document.write('<script src="src/presets-glsl.js"></script><script src="src/presets-data.js"></script><script src="src/presets-runtime.js"></script>');
